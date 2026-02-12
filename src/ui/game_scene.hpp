6b1b091bebc9c9bdc32d525dec3edb9e2bd2d78c
Atlaxsyys <y.shafran-06@mail.ru>
2026-02-13 00:36:23 +0300

    [physics]: wired GameScene to PhysicsEngine command/update flow

--- src/ui/game_scene.hpp
+++ src/ui/game_scene.hpp
@@ -1,6 +1,9 @@
 #pragma once
+#include "data/level_loader.hpp"
+#include "physics/physics_engine.hpp"
 #include "render/renderer.hpp"
 #include "scene.hpp"
+#include "shared/thread_safe_queue.hpp"
 #include "shared/world_snapshot.hpp"
 #include "ui/slingshot.hpp"
 
@@ -12,9 +15,13 @@ class GameScene : public Scene
 private:
     Renderer renderer_;
     Slingshot slingshot_;
+    PhysicsEngine physics_;
+    ThreadSafeQueue<Command> command_queue_;
+    LevelLoader level_loader_;
     WorldSnapshot snapshot_;
     sf::Font font_;
     sf::Text hud_text_;
+    sf::Clock frame_clock_;
 
     static WorldSnapshot make_mock_snapshot();
 
